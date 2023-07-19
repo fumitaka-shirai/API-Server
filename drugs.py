bc6e299c7969af1b5d770dceec9e88d73f8f1e0f
fumitaka-shirai <130987855+fumitaka-shirai@users.noreply.github.com>
2023-07-19 20:01:16 +0900

    APIサーバーの統一

--- drugs.py
+++ drugs.py
@@ -1,128 +1,105 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 import sqlite3
+import logging
 
 conn = sqlite3.connect('drugs.db')
 c = conn.cursor()
+logging.basicConfig(filename='error_log.txt', level=logging.ERROR)
 
-urls = [
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00059041',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00067912',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00055886',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00056315',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00050617',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00057119',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00053562',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00051194',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00058267',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00054823',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070777',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00048083',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00065375',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00059506',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062726',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00054178',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00068210',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00065322',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061681',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00052038',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062425',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062803',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00066927',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00068932',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00067321',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061194',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00060553',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061470',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00060545',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00052465',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00060549',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00066181',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062255',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00063406',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00067494',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061859',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00063434',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062589',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00058959',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00060048',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00059507',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070761',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00056791',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070477',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00059233',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00068720',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070070',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00067659',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00067301',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061180',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061213',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00058280',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00061437',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00062353',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00070680',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00056175',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00060403',
-    'https://www.kegg.jp/medicus-bin/japic_med?japic_code=00012985',
+try:
+    urls = []
+    for i in range(380, 15000):
+        japic_code = str(i).zfill(8)  # 8桁の0パディングされたJapicコードを生成
+        url = f'https://www.kegg.jp/medicus-bin/japic_med?japic_code={japic_code}'
+        urls.append(url)
 
-]
+    for url in urls:
+        res = requests.get(url)
+        res.encoding = 'UTF-8'
+        soup = BeautifulSoup(res.text, 'html.parser')
 
-for url in urls:
-    res = requests.get(url)
-    res.encoding = 'UTF-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
+        title_element = soup.title
+        if title_element is not None:
+            title_text = title_element.get_text()
+            if " : " in title_text:
+                name = title_text.split(" : ")[1]
+            else:
+                name = "No Title Available"
+        else:
+            name = "No Title Available"
 
-    name = soup.title.get_text().split(" : ")[1]
-    print(name)
+        category = ''
 
+        th_tags = soup.find_all('th')
+        td_tags = soup.find_all('td')
 
-    th_tags = soup.find_all('th')
-    td_tags = soup.find_all('td')
+        for th in th_tags:
+            if th.get_text() == '薬効分類名':
+                next_td = th.find_next_sibling('td')
+                if next_td:
+                    category = next_td.get_text()
+                    print(category)
 
-    for th in th_tags:
-        if th.get_text() == '薬効分類名':
-            next_td = th.find_next_sibling('td')
-            if next_td:
-                category = next_td.get_text()
-                print(category)
-                
-    td_tags = soup.find_all('td', class_='title')
+        td_tags = soup.find_all('td', class_='title')
 
-    for td in td_tags:
-        if td.get_text() == '薬効分類名':
-            next_td = td.find_next_sibling('td', class_='item')
-            if next_td:
-                category = next_td.get_text()
-                print(category)  
-                
+        for td in td_tags:
+            if td.get_text() == '薬効分類名':
+                next_td = td.find_next_sibling('td', class_='item')
+                if next_td:
+                    category = next_td.get_text()
+                    print(category)
 
-    dose = ""
-    
-    h4_tags = soup.find_all('h4', class_='contents-title', id='par-6')
-    div_tags = soup.find_all('div', class_='contents-block')
+        dose = ""
 
+        h5_tags = soup.find_all('h5')
+        h4_tags = soup.find_all('h4', class_='contents-title', id='par-6')
+        div_tags = soup.find_all('div', class_='contents-block')
 
-    dose_list = []
+        for h5 in h5_tags:
+            if '用法及び用量' in h5.get_text():
+                next_div = h5.find_next_sibling('div', class_='block1')
+                if next_div:
+                    dose = next_div.get_text()
+                    print(dose)
 
-    for h4 in h4_tags:
-        if '用法及び用量' in h4.get_text():
-           next_div = h4.find_next_sibling('div', class_='contents-block')
-           if next_div:
-            dose = next_div.get_text()
-            print(dose)
+        for h4 in h4_tags:
+            if '用法及び用量' in h4.get_text():
+                next_div = h4.find_next_sibling('div', class_='contents-block')
+                if next_div:
+                    dose = next_div.get_text()
+                    print(dose)
 
-    h5_tags = soup.find_all('h5')
-    div_tags = soup.find_all('div',class_='block1')
+        h5_tags = soup.find_all('h5')
+        div_tags = soup.find_all('div', class_='block1')
 
-    for h5 in h5_tags:
-        if '用法用量' in h5.get_text():
-          next_div = h5.find_next_sibling('div', class_='block1')
-          if next_div:
-            for p in next_div.find_all('p'):
-                    dose += p.get_text() + "\n"  # 各段落を改行で連結
-            print(dose.strip())
-             
-    c.execute("INSERT INTO drugs (name, category,dose) VALUES (?, ?, ?)",(name, category, dose))
-    conn.commit()
+        for h5 in h5_tags:
+            if '用法用量' in h5.get_text():
+                next_div = h5.find_next_sibling('div', class_='block1')
+                if next_div:
+                    for p in next_div.find_all('p'):
+                        dose += p.get_text() + "\n"  # 各段落を改行で連結
+                    print(dose.strip())
+
+        for h5 in h5_tags:
+            if '用法・用量' in h5.get_text():
+                next_div = h5.find_next_sibling('div', class_='block1')
+                if next_div:
+                    for p in next_div.find_all('p'):
+                        dose += p.get_text() + "\n"  # 各段落を改行で連結
+                    print(dose.strip())
+        
+
+                    
+
+        c.execute("INSERT INTO drugs (name, category, dose) VALUES (?, ?, ?)", (name, category, dose))
+        conn.commit()
+
+        time.sleep(5)
+
+except Exception as e:
+    # エラーメッセージをログに書き込む
+    logging.error("書き込みが出来ませんでした")
 
 conn.close()
