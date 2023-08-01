da31b3623c45c276cb54ea8618458a8c840addc1
fumitaka-shirai <130987855+fumitaka-shirai@users.noreply.github.com>
2023-08-02 08:39:20 +0900

    detileのAPIサーバー実装

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import sqlite3
 app = Flask(__name__)
 CORS(app)
 
+def get_detiles():
+    conn = sqlite3.connect('drugs.db')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM detiles')
+
+    detiles = cursor.fetchall()
+    conn.close()
+
+    return detiles 
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
@@ -74,6 +84,38 @@ def add_chat():
         'drug_id': drug_id  
     }
     return jsonify(message)
+
+@app.route('/api/detiles', methods=['GET'])
+def get_detiles():
+    conn = sqlite3.connect('drugs.db')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM detiles')
+
+    detiles = cursor.fetchall()
+    conn.close()
+
+    detiles_list = []  # リストを初期化
+
+    for data in detiles:  # リスト内の各データを個別に処理
+        detile = {
+            'id': data['id'],
+            'name': data['name'],
+            'indication': data['indication'],
+            'interations': data['interations'],
+            'sideeffects': data['sideeffects'],
+            'pharmacology': data['pharmacology'],
+            'drug_id': data['drug_id']
+        }
+        detiles_list.append(detile)
+
+    if detiles_list:
+        # 薬の詳細情報が見つかった場合はJSON形式に整形して返す
+        return jsonify(detiles_list)
+    else:
+        # データが見つからなかった場合はエラーを返す
+        return jsonify({'error': 'Detiles not found'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
